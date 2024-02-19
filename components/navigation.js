76e5947f5dc9397498bc9dde2ca216cafcbe5b87
Mustafa Akaydın <akaydinmustafa5@gmail.com>
2024-02-20 01:49:06 +0300

    fix router

--- components/navigation.js
+++ components/navigation.js
@@ -3,30 +3,32 @@ import { useRouter } from "next/router";
 
 import { MENU } from "../constants";
 import styles from "./navigation.module.css";
+import cn from "classnames";
 
 import NavigationButton from "./navigation-button";
 import TextTitle from "./text-title";
 
 import { Twitter } from "./icons";
 
-export function Navigation({ flat = false}) {
+function Navigation({ flat = false }) {
   const router = useRouter();
-  
+
   return (
     <nav className={styles.nav}>
       <NavigationButton>
         <Twitter style={{ fontSize: 30 }} />
       </NavigationButton>
       {MENU.map((menu) => {
-        const showTitle = !flat || menu.title.length === 0;
-        const selected = router.asPath === menu.path;
+        const showTitle = !flat && menu.title.length > 0;
+        const selected = router.pathname === menu.path;
+
         return (
           <NavigationButton
             key={menu.key}
             notify={menu.notify}
             selected={selected}
             href={menu.path}
-            className={styles.navButton}
+            className={cn(styles.navButton, menu.key)}
           >
             {selected ? menu.iconSelected : menu.icon}
             {showTitle && <TextTitle>{menu.title}</TextTitle>}
